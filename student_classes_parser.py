6bb3ae4a7c744ea1a0e82d694143541fe7758722
State High <theyogang860@gmail.com>
2022-04-23 14:10:24 +1000

    init

---
+++ student_classes_parser.py
@@ -0,0 +1,75 @@
+from glob import glob
+import json
+import csv
+import re
+
+"""Settings"""
+name_half = r"([a-zA-Z\s\'\(\)-]+)"
+name_format = fr"{name_half},\s{name_half},"
+room_format = r"[A-Z][G\d].?\d\d\Z|LIB[A-D]\Z|OVAL[A-D]\Z|OVLJ|POOL"
+arc_class_format = r"([7-9]|10|11|12)[A-Z]"
+period_columns = 1, 3, 5, 7
+
+tickets = []
+
+for file_name in glob("student_timetables/*.csv"):
+    with open(file_name) as file:
+        reader = csv.reader(file)
+        rows = [row for row in reader if row != ['', '', '', '', '', '', '', '', '']]
+
+        for row_index in range(len(rows)):
+            # do a traditional for loop so that the next row can be found
+            row = rows[row_index]
+
+            name = re.match(name_format, row[0])
+            if name:
+                ticket = {}
+
+                student_name = f"{name.group(2)} {name.group(1)}"
+                arc_class = re.search(arc_class_format, row[0]).group(0)
+                student_id = f"{student_name} [{arc_class}]"
+
+                ticket['StudentName'] = student_name
+                ticket['StudentID'] = student_id
+
+                # print(student_name, arc_class)
+                for period, period_column in enumerate(period_columns):
+                    period = period + 1
+                    room = re.search(room_format, row[period_column])
+                    if room:
+                        ticket[f"P{period}"] = room.group(0)
+                    else:
+                        # print("NO CLASS", row)
+                        ticket[f"P{period}"] = "NONE"
+                        # if a person's class got split across two rows, try to find the class in the next row
+                        next_row = rows[row_index + 1]
+                        if next_row[0] == "":
+                            # checks if the next row is the second half of this row and not a new person
+                            room = re.search(room_format, next_row[period_column])
+                            if room:
+                                ticket[f"P{period}"] = room.group(0)
+                                # print(room.group(0))
+                            else:
+                                # print("NO ROOM")
+                                pass
+
+                tickets.append(ticket)
+                print(ticket)
+            else:
+                # print(row)
+                pass
+
+# write classes to csv
+with open('students_classes.csv', 'w') as file:
+    fieldnames = ["StudentID", "StudentName", "P1", "P2", "P3", "P4"]
+    writer = csv.DictWriter(file, fieldnames)
+    for ticket in tickets:
+        writer.writerow(ticket)
+
+# write only names to json so that ticket inputter use them
+with open('student_names.json', 'w') as file:
+    name_list = [ticket['StudentID'] for ticket in tickets]
+    json.dump(name_list, file, indent=4)
+
+# longest name:
+# Danthila Jayasinghe Mudiyanselage [7P]
